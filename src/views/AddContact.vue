d086c81809ceb3bb8ffa9bed9621498a45c152be
charonchai1 <charonchai2541@gmail.com>
2023-05-19 11:59:18 +0700

    add new create contact path

--- src/views/AddContact.vue
+++ src/views/AddContact.vue
@@ -1,9 +1,46 @@
 <template>
-    <h2>Add Contact</h2>
+  <div class="container mt-3">
+    <div class="row">
+      <div class="col">
+        <p class="h3 text-success fw-bold">Add Contact</p>
+        <p class="fst-italic">djwqdklqklwqjklwqjklwqjkl</p>
+      </div>
+    </div>
+  </div>
+  <div class="container mt-3">
+    <div class="row">
+      <div class="col-md-4">
+        <form>
+          <div class="mb-2">
+            <input type="text" class="form-control" placeholder="Full Name" />
+          </div>
+          <div class="mb-2">
+             <label for="birthDate">Date of Birth:</label>
+    <input type="date" id="birthDate" v-model="birthDate">
+          </div>
+          <div class="mb-2">
+            <input type="number" class="form-control" placeholder="Mobile" />
+          </div>
+          <div class="mb-2">
+            <input type="text" class="form-control" placeholder="Address" />
+          </div>
+          <div class="mb-2">
+            <input type="text" class="form-control" placeholder="Username" />
+          </div>
+          <div class="mb-2">
+            <input type="text" class="form-control" placeholder="Password" />
+          </div>
+          <div class="mb-2">
+            <input type="submit" class="btn btn-success" value="Create">
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: "AddContact"
-}
+  name: "AddContact",
+};
 </script>
--- src/views/ContactManager.vue
+++ src/views/ContactManager.vue
@@ -32,47 +32,56 @@
   </div>
   <div class="container mt-3">
     <div class="row">
-        <div class="col-md-6">
-            <div class="card my-2 list-group-item-success">
-                <div class="card-body">
-                    <div class="row align-items-center ">
-                        <div class="col-sm-4" >
-                            <img src="https://img.icons8.com/?size=512&id=QYqNSpKRKelP&format=png" alt="" class="contact-img">
-                        </div>
-                        <div class="col-sm-7">
-                                <ul class="list-group">
-                                    <li class="list-group-item">
-                                        Name :
-                                        <span class="fw-bold">Name</span>
-                                    </li>
-                                     <li class="list-group-item">
-                                        BirthDate :
-                                        <span class="fw-bold">BirthDate</span>
-                                    </li>
-                                    <li class="list-group-item">
-                                        Mobile :
-                                        <span class="fw-bold">Mobile</span>
-                                    </li>
-                                   
-                                </ul>
-                        </div>
-                        <div class="col-sm-1 d-flex flex-column justify-content-center align-items-center">
-                             <router-link to="/contacts/view/:contactId"
-                             class="btn btn-warning my-1">
-                              <i class="fa fa-eye"></i>
-                             </router-link>
-                             <router-link to="/contacts/edit/:contactId"
-                             class="btn btn-primary my-1">
-                              <i class="fa fa-pen"></i>
-                             </router-link>
-                                <button class="btn btn-danger my-1">
-                                    <i class="fa fa-trash"></i>
-                                </button>
-                        </div>
-                    </div>
-                </div>
+      <div class="col-md-6">
+        <div class="card my-2 list-group-item-success shadow-lg">
+          <div class="card-body">
+            <div class="row align-items-center">
+              <div class="col-sm-4">
+                <img
+                  src="https://img.icons8.com/?size=512&id=QYqNSpKRKelP&format=png"
+                  alt=""
+                  class="contact-img"
+                />
+              </div>
+              <div class="col-sm-7">
+                <ul class="list-group">
+                  <li class="list-group-item">
+                    Name :
+                    <span class="fw-bold">Name</span>
+                  </li>
+                  <li class="list-group-item">
+                    BirthDate :
+                    <span class="fw-bold">BirthDate</span>
+                  </li>
+                  <li class="list-group-item">
+                    Mobile :
+                    <span class="fw-bold">Mobile</span>
+                  </li>
+                </ul>
+              </div>
+              <div
+                class="col-sm-1 d-flex flex-column justify-content-center align-items-center"
+              >
+                <router-link
+                  to="/contacts/view/:contactId"
+                  class="btn btn-warning my-1"
+                >
+                  <i class="fa fa-eye"></i>
+                </router-link>
+                <router-link
+                  to="/contacts/edit/:contactId"
+                  class="btn btn-primary my-1"
+                >
+                  <i class="fa fa-pen"></i>
+                </router-link>
+                <button class="btn btn-danger my-1">
+                  <i class="fa fa-trash"></i>
+                </button>
+              </div>
             </div>
+          </div>
         </div>
+      </div>
     </div>
   </div>
 </template>
